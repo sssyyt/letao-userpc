cc34dbff568d18d1d706c970db36882be89962cd
shiyutian <2602068209@qq.com>
2023-08-31 16:00:20 +0800

    完成吸顶导航功能

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,3 +1,4 @@
 <template>
   我是home
+  <div style="height: 1000px;"></div>
 </template>
---
+++ src/views/Layout/components/LayoutFixed.vue
@@ -0,0 +1,74 @@
+<script setup>
+import LayoutHeader from './LayoutHeader.vue'
+// vueUse
+import { useScroll } from '@vueuse/core'
+const { y } = useScroll(window)
+
+
+</script>
+
+<template>
+  <div class="app-header-sticky" :class="{ show: y > 78 }">
+    <div class="container">
+      <RouterLink class="logo" to="/" />
+      <!-- 导航区域 -->
+
+      <LayoutHeader />
+    
+    </div>
+  </div>
+</template>
+
+
+<style scoped lang='scss'>
+.app-header-sticky {
+  width: 100%;
+  height: 102px;
+  position: fixed;
+  left: 0;
+  top: 0;
+  z-index: 999;
+  background-color: #fff;
+  
+  
+  transform: translateY(-100%);
+  opacity: 0;
+
+  &.show {
+    transition: all 0.3s linear;
+    transform: none;
+    opacity: 1;
+  }
+
+  .container {
+    display: flex;
+    align-items: center;
+  }
+
+  .logo {
+    width: 200px;
+    height: 80px;
+    background: url("@/assets/images/logo.png") no-repeat right 2px;
+    background-size: 160px auto;
+  }
+
+  .right {
+    width: 220px;
+    display: flex;
+    text-align: center;
+    padding-left: 40px;
+    border-left: 2px solid $letaoColor;
+
+    a {
+      width: 38px;
+      margin-right: 40px;
+      font-size: 16px;
+      line-height: 1;
+
+      &:hover {
+        color: $letaoColor;
+      }
+    }
+  }
+}
+</style>
