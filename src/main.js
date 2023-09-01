831d670887b73b17844f013ba97f9187ab8a244c
shiyutian <2602068209@qq.com>
2023-09-01 09:54:49 +0800

    懒加载封装为插件

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 import './assets/main.css'
-import { useIntersectionObserver } from '@vueuse/core'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import { lazyPlugin } from '@/directives'
 import App from './App.vue'
 import router from './router'
 
@@ -13,24 +12,7 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(lazyPlugin)
 app.mount('#app')
 
-app.directive('img-lazy', {
-    mounted(el, binding) {
-        // el: 指令绑定的那个元素 img
-        // binding: binding.value  指令等于号后面绑定的表达式的值  图片url
-        // console.log(el, binding.value)
-        const { stop } = useIntersectionObserver(
-            el,
-            ([{ isIntersecting }]) => {
-                console.log(isIntersecting)
-                if (isIntersecting) {
-                    // 进入视口区域
-                    el.src = binding.value
-                    stop()
-                }
-            },
-        )
-    }
-})
+
